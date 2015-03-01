cae32af58aafa4add4be53bc5d1b2c86f595db91
fred <fw1714@mun.ca>
2015-03-01 10:03:54 -0800

    template

---
+++ src/main/java/panda/Application.java
@@ -0,0 +1,16 @@
+package panda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Feng on 01-Mar-15.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
