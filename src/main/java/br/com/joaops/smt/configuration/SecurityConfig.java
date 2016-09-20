9280a16b64de2724755df1eec90bbac947a75304
João Paulo <Joo Paulo@192.168.86.100>
2016-09-20 09:29:18 -0300

    Alterado o nome da classe WebSecurityConfig para SecurityConfig; Alterado a Classe SecurityConfig, classe responsável por fazer a segurança da aplicação, mas não foi totalmente configurada;

--- src/main/java/br/com/joaops/smt/configuration/SecurityConfig.java
+++ src/main/java/br/com/joaops/smt/configuration/SecurityConfig.java
@@ -2,48 +2,42 @@ package br.com.joaops.smt.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.stereotype.Service;
 
 /**
  * Created by Feng on 01-Mar-15.
  */
 
 @Configuration
-@EnableWebMvcSecurity
-@ComponentScan(
-        basePackages = {"br.com.joaops.smt.repository", "br.com.joaops.smt.service"},
-        includeFilters = @ComponentScan.Filter(Service.class)
-)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/admin").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .and()
-                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .invalidateHttpSession(true);
-    }
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Autowired
-    //@Qualifier
     UserDetailsService userDetailsService;
     
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").authenticated()
+                .anyRequest().authenticated();
+        
+        http.formLogin();
+        
+        http.logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .invalidateHttpSession(true);
     }
     
     static public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
